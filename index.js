d0a7b4ad04862e07602f07857609a1491f054ab5
Rhys <rhys@rhysjenkins.uk>
2025-02-03 23:41:51 +0000

    Comment, but I'm only human

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function readFileAsync(filePath) {
 }
 
 app.get("/europeRandom", async (req, res) => {
+    // The request needs to contain auth data
     try {
         const europeData = await readFileAsync("./data/europe.json");
         console.log("europe data: ", europeData);
