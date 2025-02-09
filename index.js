27ad228defc2d3edfc2b52ad9bea28019a8f5f89
Rhys <rhys@rhysjenkins.uk>
2025-02-09 12:15:45 +0000

    Added routes, changed names

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 // For the moment, I've removed the following in order to test from my local machine: { origin: "https://cities.rhysjenkins.uk" }
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello world!");
 });
 
 async function readFileAsync(filePath) {
@@ -20,10 +20,27 @@ async function readFileAsync(filePath) {
     }
 }
 
-app.get("/europeRandom", async (req, res) => {
-    // The request needs to contain auth data
+app.get("/europe", async (req, res) => {
     try {
         const europeData = await readFileAsync("./data/europe.json");
+        res.send(europeData);
+    } catch (error) {
+        throw error;
+    }
+});
+
+app.get("/asia", async (req, res) => {
+    try {
+        const asiaData = await readFileAsync("./data/asia.json");
+        res.send(europeData);
+    } catch (error) {
+        throw error;
+    }
+});
+
+app.get("/africa", async (req, res) => {
+    try {
+        const africaData = await readFileAsync("./data/africa.json");
         res.send(europeData);
     } catch (error) {
         throw error;
