6bd9ad8b4a992c0ba003e2f80b7fdfbcdf395c23
Rhys <rhys@rhysjenkins.uk>
2025-03-11 17:57:47 +0900

    Signup validation done

--- index.js
+++ index.js
@@ -33,27 +33,48 @@ app.post(
     jsonParser,
     [
         body("firstName")
-            .trim()
+            .trim() // Always call .trim before calling .notEmpty in order to remove whatspace before checking if the field is empty
             .notEmpty()
-            .withMessage("Last name is required")
-            .isAlpha("en-GB", { ignore: " -'" })
+            .withMessage("The first name field is required")
+            .isAlpha("en-GB", { ignore: " -'" }) // This string allows spaces, hyphens, and apostrophes
             .withMessage(
-                "Last name must contain only letters, spaces, hyphens, or apostrophes"
+                "The first name field must contain only letters, spaces, hyphens, or apostrophes"
             )
             .escape(),
         body("lastName")
             .trim()
             .notEmpty()
-            .withMessage("Last name is required")
+            .withMessage("The last name field is required")
             .isAlpha("en-GB", { ignore: " -'" })
             .withMessage(
-                "Last name must contain only letters, spaces, hyphens, or apostrophes"
+                "The last name field must contain only letters, spaces, hyphens, or apostrophes"
             )
             .escape(),
         body("email")
             .trim()
             .isEmail()
             .withMessage("Please enter a valid email address."),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Please enter a password.")
+            .isLength({ min: 10 })
+            .withMessage("Your password must contain at least ten characters.")
+            .matches(/[A-Z]/)
+            .withMessage(
+                "Your password must contain at least one uppercase letter."
+            )
+            .matches(/[a-z]/)
+            .withMessage(
+                "Your password must contain at least one lowercase letter."
+            )
+            .matches(/[0-9]/)
+            .withMessage("Your password must contain at least one number.")
+            .matches(/[\W_]/)
+            .withMessage(
+                "Your password must contain at least one special character."
+            )
+            .escape(),
     ],
     (req, res) => {
         const errors = validationResult(req);
@@ -61,38 +82,12 @@ app.post(
             return res.status(400).json({ errors: errors.array() });
         } else {
             res.send({
-                message: "Inputs valid",
+                message: "All inputs are valid",
                 body: req.body,
             });
         }
     }
 );
-
-// app.post(
-//     "/register",
-//     check("email")
-//         .isEmail()
-//         .withMessage("Enter a valid email address")
-//         .normalizeEmail(),
-//     check("first_name")
-//         .not()
-//         .isEmpty()
-//         .withMessage("Your first name is required")
-//         .trim()
-//         .escape(),
-//     check("last_name")
-//         .not()
-//         .isEmpty()
-//         .withMessage("Your last name is required")
-//         .trim()
-//         .escape(),
-//     check("password")
-//         .notEmpty()
-//         .isLength({ min: 8 })
-//         .withMessage("Must be at least 8 chars long"),
-//     Validate,
-//     Register
-// );
 
 app.get("/europe", async (req, res) => {
     try {
