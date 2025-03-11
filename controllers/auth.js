e73b72774c26f1e4524b690c2de43abad3e8f3b8
Rhys <rhys@rhysjenkins.uk>
2025-03-11 18:54:22 +0900

    Errors in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,29 +13,32 @@ async function Register(req, res) {
             password,
         });
         // Check if user already exists
+        console.log("The User object:", User);
         const existingUser = await User.findOne({ email });
-        if (existingUser)
+        if (existingUser) {
             return res.status(400).json({
                 status: "failed",
                 data: [],
                 message:
                     "It seems you already have an account. Please log in instead.",
             });
-        const savedUser = await newUser.save(); // save new user into the database
-        const { role, ...user_data } = savedUser._doc;
-        res.status(200).json({
-            status: "success",
-            data: [user_data],
-            message:
-                "Thank you for registering with us. Your account has been successfully created.",
-        });
+        } else {
+            const savedUser = await newUser.save(); // save new user into the database
+            const { role, ...user_data } = savedUser._doc;
+            res.status(200).json({
+                status: "success",
+                data: [user_data],
+                message:
+                    "Thank you for registering with us. Your account has been successfully created.",
+            });
+        }
     } catch (error) {
         res.status(500).json({
             status: "error",
             code: 500,
             data: [],
             message: "Internal Server Error",
-            error: error,
+            error: { message: error.message, stack: error.stack },
         });
     }
     res.end();
