668907cc324a7922fe4f368e0a927777e15e126a
Rhys <rhys@rhysjenkins.uk>
2025-03-19 14:18:14 +0900

    Updating cors options

--- index.js
+++ index.js
@@ -11,7 +11,25 @@ const Validate = require("./middleware/validate.js");
 const { body, validationResult } = require("express-validator");
 const databaseConnect = require("./database/db.js");
 
-app.use(cors());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://cities.rhysjenkins.uk",
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 // For the moment, I've removed the following in order to test from my local machine: { origin: "https://cities.rhysjenkins.uk" }
 
 // Connect to the database
