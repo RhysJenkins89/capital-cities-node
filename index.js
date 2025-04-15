998e4f86979dc30f319e5a8c3c9a96cc2a9252b4
Rhys <rhys@rhysjenkins.uk>
2025-04-15 12:03:21 +0100

    Now fetching Europe data from mongodb

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const Validate = require("./middleware/validate.js");
 const { body, validationResult } = require("express-validator");
 const databaseConnect = require("./database/db.js");
 
+// I'm trying what might be a more intuitive way of getting to the database here
+const { MongoClient } = require('mongodb');
+
 const allowedOrigins = [
     "http://localhost:5173",
     "https://cities.rhysjenkins.uk",
@@ -33,7 +36,7 @@ app.use(cors(corsOptions));
 // For the moment, I've removed the following in order to test from my local machine: { origin: "https://cities.rhysjenkins.uk" }
 
 // Connect to the database
-databaseConnect();
+// databaseConnect();
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -110,20 +113,13 @@ app.get("/europe", async (req, res) => {
     try {
         const databasePassword = process.env.mongoPassword;
         const uri = `mongodb+srv://rhysjenkins89:${databasePassword}@capital-cities-site.z6o7t.mongodb.net/?retryWrites=true&w=majority&appName=capital-cities-site`;
-
-        const continentsConnection = mongoose.createConnection(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: 'continents'
-        });
-
-        const countrySchema = new mongoose.Schema({
-            capital: String,
-            definiteArticle: Boolean
-        });
-
-        // const europeData = await readFileAsync("./data/europe.json"); // fetch from the database here
-        // res.send(europeData);
+        const mongoClient = new MongoClient(uri);
+        await mongoClient.connect();
+        const continentsDb = mongoClient.db('continents');
+        const europeData = continentsDb.collection('europe');
+        const countriesData = await europeData.find({}).toArray();
+        delete countriesData[0]['_id'];
+        res.send(countriesData);
     } catch (error) {
         throw error;
     }
