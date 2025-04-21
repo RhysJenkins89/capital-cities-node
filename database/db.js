c785e5f1caa2f83ff02ca4c7ce82718f903e43a4
Rhys <rhys@rhysjenkins.uk>
2025-04-21 23:20:58 +0100

    Updating db connection

--- database/db.js
+++ database/db.js
@@ -2,26 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const databasePassword = process.env.mongoPassword;
 const uri = `mongodb+srv://rhysjenkins89:${databasePassword}@capital-cities-site.z6o7t.mongodb.net/?retryWrites=true&w=majority&appName=capital-cities-site/continents`;
-// const { MongoClient } = require("mongodb");
 
 async function databaseConnect() {
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(uri);
         console.log("MongoDB connected successfully.");
     } catch (error) {
         console.error("MongoDB connection error:", error);
-        process.exit(1);
     }
-    // try {
-    //     const mongoClient = new MongoClient(uri);
-    //     await mongoClient.connect();
-    //     console.log("MongoDB connected successfully.");
-    // } catch (error) {
-    //     console.error("MongoDB connection error:", error);
-    // }
 }
 
 module.exports = databaseConnect;
