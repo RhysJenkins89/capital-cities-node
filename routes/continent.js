0ac4f57c26f42c02135ef6522ff09a667845a1eb
Rhys <rhys@rhysjenkins.uk>
2025-05-14 22:39:53 +0100

    Added error handling and note to route

--- routes/continent.js
+++ routes/continent.js
@@ -4,8 +4,21 @@ const mongoose = require("mongoose");
 const CountrySchema = require("../models/Country");
 const capitaliseFirstLetter = require("../utils/capitaliseFirstLetter");
 
+const validContinentRoutes = [
+    "europe",
+    "asia",
+    "africa",
+    "north-america",
+    "south-america",
+    "oceania",
+];
+
 router.get("/:continent", async (req, res) => {
     const continentName = req.params.continent;
+
+    if (!validContinentRoutes.includes(continentName)) {
+        return res.status(400).json({ error: `Invalid continent: '${continentName}'` });
+    }
 
     try {
         const modelName = capitaliseFirstLetter(continentName);
@@ -15,6 +28,8 @@ router.get("/:continent", async (req, res) => {
 
         const countries = await ContinentModel.find().lean();
 
+        // My theory: mongoose is searching for the collection. It doesn't exist. Instead of sending back an error, it creates the collection and sends back the data, which in this case is an empty array.
+
         res.send(countries);
     } catch (error) {
         console.error("Error fetching countries:", error);
@@ -23,3 +38,7 @@ router.get("/:continent", async (req, res) => {
 });
 
 module.exports = router;
+
+// Using Insomnia, I sent a get request to the /ocenia route, which is a typo. The correct route is oceania. Doing so posted an empty collection to my MongoDB. What is going on?
+// Moreover, hitting an undefined route with a get request returns an empty array, instead of a properly handled error.
+// This is it: somehow, the get request is posting to my MongoDB. This is weird.
