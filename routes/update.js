e1841fd4d208a93d254dbfd0b333ece5ed02445d
Rhys <rhys@rhysjenkins.uk>
2025-05-23 09:51:17 +0100

    Sorting update route

--- routes/update.js
+++ routes/update.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const CountrySchema = require("../models/Country"); // I want the continent model, not the country schema.
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+// const CountrySchema = require("../models/Country"); // I want the continent model, not the country schema.
 
-router.put("/update", async (req, res) => {
+router.put("/update", jsonParser, async (req, res) => {
     console.log("This is a put request on the /update route.");
+    console.log("Req:", req.body);
     res.json(res.body);
 });
 
