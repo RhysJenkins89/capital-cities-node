4656e9469a3e6da3aec0db0d10715912258599db
Rhys <rhys@rhysjenkins.uk>
2025-06-04 10:59:29 +0100

    Working on update route

--- routes/update.js
+++ routes/update.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+const mongoose = require("mongoose");
 // const CountrySchema = require("../models/Country"); // I want the continent model, not the country schema.
 
 router.put("/update", jsonParser, async (req, res) => {
     console.log("This is a put request on the /update route.");
-    // console.log("Req:", req.body);
-    console.log("Res:", req.body);
+
+    const database = mongoose.connection.db;
+    const result = await database
+        .collection(req.body.continent)
+        .updateOne({ _id: req.body.countryId }, [{ confidenceIndex: req.body.userConfidence }]);
+
+    console.log("Update result:", result);
+
+    console.log("Req:", req.body);
     res.json(req.body);
 });
 
