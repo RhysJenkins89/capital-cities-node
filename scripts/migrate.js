be0c72e4d0c83651ad26c4732f856ac77c807586
Rhys <rhys@rhysjenkins.uk>
2025-06-13 14:46:43 +0100

    Fighting with mongo auth

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,40 +1,56 @@
 // This script updates my MongoDB structure. From multiple continent collections, I'll go to a single countries collection. Each country document will feature a continent property.
 
 const mongoose = require("mongoose");
-const databaseConnect = require("../database/db");
+// const databaseConnect = require("../database/db");
 
 const continentNames = ["europe", "africa", "asia", "north-america", "south-america", "oceania"];
 
 const countrySchema = new mongoose.Schema({}, { strict: false });
 const Country = mongoose.model("Country", countrySchema, "countries");
+const databasePassword = process.env.mongoPassword;
 
 (async () => {
     try {
-        await databaseConnect();
-        if (mongoose.connection.readyState === 1) {
-            console.log("Connected to the Mongo database.");
-            for (const continent of continentNames) {
-                const SourceModel = mongoose.model(`${continent}_model`, countrySchema, continent);
-                const docs = await SourceModel.find({});
-                console.log("docs:", docs);
-                console.log(`Migrating ${docs.length} documents from ${continent}...`);
-                const modifiedDocs = docs.map((doc) => {
-                    const obj = doc.toObject();
-                    obj.continent = continent;
-                    delete obj._id; // Let MongoDB assign new _id in the new collection
-                    return obj;
-                });
-                // console.log("modifiedDocs:", modifiedDocs);
-                if (modifiedDocs.length > 0) {
-                    await Country.insertMany(modifiedDocs);
-                }
+        const sourceDatabase = await mongoose.createConnection(
+            `mongodb+srv://rhysjenkins89:${databasePassword}@capital-cities-site.z6o7t.mongodb.net/continents?retryWrites=true&w=majority&appName=capital-cities-site`
+        );
+        const targetDatabase = await mongoose.createConnection(
+            `mongodb+srv://rhysjenkins89:${databasePassword}@capital-cities-site.z6o7t.mongodb.net/countriesDatabase?retryWrites=true&w=majority&appName=capital-cities-site`
+        );
+        const TargetCountry = targetDatabase.model(
+            "Country",
+            new mongoose.Schema({
+                name: String,
+                capital: String,
+                definiteArticle: Boolean,
+                continent: String,
+                confidenceIndex: Number,
+            })
+        );
+        for (const continent of continentNames) {
+            const SourceModel = sourceDatabase.model(
+                `${continent}_model`,
+                countrySchema,
+                continent
+            );
+            const docs = await SourceModel.find({});
+            console.log(`Migrating ${docs.length} from ${continent}`);
+            const modifiedDocs = docs.map((doc) => {
+                const obj = doc.toObject();
+                obj.continent = continent;
+                delete obj._id; // Avoid _id conflict
+                return obj;
+            });
+            if (modifiedDocs.length > 0) {
+                await TargetCountry.insertMany(modifiedDocs);
             }
-            console.log("Migration complete");
         }
+        console.log("Migration complete.");
     } catch (error) {
         console.log("Something went wrong:", error);
     } finally {
-        mongoose.disconnect();
+        await sourceDatabase.close();
+        await targetDatabase.close();
         console.log("Disconnected from the DB.");
     }
 })();
