275a0f55f8d4e71f27934478ff730129992adf8b
Rhys <rhys@rhysjenkins.uk>
2025-06-14 20:49:43 +0100

    Updated and removed models

--- models/Country.js
+++ models/Country.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const CountrySchema = new mongoose.Schema({
     name: String,
     capital: String,
+    continent: String,
     definiteArticle: Boolean,
     confidenceIndex: Number,
 });
 
 // module.exports = mongoose.model("Country", CountrySchema)
-module.exports = CountrySchema; // Should I export a model here? At the moment I only export the schema.
+module.exports = mongoose.model("Country", CountrySchema); // Should I export a model here? At the moment I only export the schema.
--- scripts/databaseCheck.js
+++ scripts/databaseCheck.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const databasePassword = process.env.mongoPassword;
 const uri = `mongodb+srv://rhysjenkins89:${databasePassword}@capital-cities-site.z6o7t.mongodb.net/countriesDatabase?retryWrites=true&w=majority&appName=capital-cities-site`;
+const CountryModel = require("../models/Country");
 
 (async () => {
     try {
         await mongoose.connect(uri);
         if (mongoose.connection.readyState === 1) {
-            console.log(mongoose.countriesDatabase.find({}));
+            const countries = await CountryModel.find({});
+            console.log("Countries:", countries);
         }
     } catch (error) {
         console.error("Connection error:", error);
