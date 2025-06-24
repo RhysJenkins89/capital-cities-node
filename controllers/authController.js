a36c5231df424bdc3602506a1b52a86a431e884c
Rhys <rhys@rhysjenkins.uk>
2025-06-24 11:22:57 +0100

    Rebuilding signup/in functionality

---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+async function authController(res, res) {
+    console.log("This is the authController function");
+    res.send("Get request working");
+}
+
+module.exports = authController;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const continentRoutes = require("./continent");
 const updateRoute = require("./update");
+const authRoute = require("./auth");
 
-router.use("/", continentRoutes, updateRoute);
+router.use("/", authRoute, continentRoutes, updateRoute);
 
 module.exports = router;
