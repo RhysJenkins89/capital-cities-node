76c7e51a915ca4dede2d8d9095f542482f0b9353
Rhys <rhys@rhysjenkins.uk>
2025-06-27 18:55:17 +0100

    Sorting validation

--- middleware/validateRegisterInput.js
+++ middleware/validateRegisterInput.js
@@ -1,12 +1,35 @@
 const { body, validationResult } = require("express-validator");
 
-function validateRegisterInput(req, res, next) {
-    console.log("This is the validateRegisterInput middleware function");
-    const { firstName } = req.body;
-    if (!firstName) {
-        return res.status(400).json({ message: "The firstName field is required." });
-    }
-    next();
-}
+const validateRegisterInput = [
+    body("firstName")
+        .trim()
+        .notEmpty()
+        .withMessage("The first name field is required")
+        .isAlpha("en-GB", { ignore: " -'" }) // This string allows spaces, hyphens, and apostrophes
+        .withMessage(
+            "The first name field must contain only letters, spaces, hyphens, or apostrophes"
+        )
+        .escape(),
+    body("lastName")
+        .trim()
+        .notEmpty()
+        .withMessage("The first name field is required")
+        .isAlpha("en-GB", { ignore: " -'" })
+        .withMessage(
+            "The first name field must contain only letters, spaces, hyphens, or apostrophes"
+        )
+        .escape(),
+    (req, res, next) => {
+        const result = validationResult(req);
+        console.log("result:", result);
+
+        if (result.isEmpty()) {
+            console.log("No errors found.");
+            next();
+        } else {
+            return res.status(400).json({ errors: result.array() });
+        }
+    },
+];
 
 module.exports = validateRegisterInput;
