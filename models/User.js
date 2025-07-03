1486c49b3a3be5869106bea1c25a5628aa530592
Rhys <rhys@rhysjenkins.uk>
2025-07-03 18:55:10 +0100

    User schema

--- models/User.js
+++ models/User.js
@@ -1,24 +1,33 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
+
+// Created, updated
 
 module.exports = mongoose.model("User", UserSchema);
