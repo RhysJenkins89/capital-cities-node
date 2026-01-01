a7960f946fd28567099f2775f14eb1c1ac69cf8f
Rhys <rhys@rhysjenkins.uk>
2026-01-01 14:03:04 +0000

    Testing commit

---
+++ controllers/sign-out-controller.js
@@ -0,0 +1,6 @@
+async function signOutController(req, res) {
+    console.log("This is the sign out controller.");
+    console.log("This is the request object:", req);
+}
+
+module.exports = signOutController;
---
+++ routes/sign-out.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+const router = express.Router();
+const jsonParser = require("body-parser").json();
+const signOutController = require("../controllers/sign-out-controller");
+
+router.post("/signout", jsonParser, signOutController);
+
+module.exports = router;
