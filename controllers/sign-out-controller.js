059bbdba531d1ab5e44236b8e4e99028aea0f0a7
Rhys <rhys@rhysjenkins.uk>
2026-02-16 22:03:23 +0000

    Continue with sign in/out functionality

--- controllers/sign-out-controller.js
+++ controllers/sign-out-controller.js
@@ -1,6 +1,14 @@
+const User = require("../models/User");
+
 async function signOutController(req, res) {
     console.log("This is the sign out controller.");
     console.log("This is the request body:", req.body);
+    const userEmail = req.body.userEmail;
+    const userData = await User.findOne({ email: userEmail });
+    if (userData) {
+        console.log("userData:");
+        console.log(userData);
+    }
 }
 
 module.exports = signOutController;
